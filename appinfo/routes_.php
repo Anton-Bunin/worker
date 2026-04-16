4254bdfb06e389e37ccd4b7b62de094d8b572e48
Anton Bunin <cccpnowar@gmail.com>
2026-04-16 15:21:32 +0300

    Delete appinfo/routes_.php

--- appinfo/routes_.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'routes' => [
-        // Маршрут для главной страницы
-        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-        // Маршрут для сохранения бронирования
-        ['name' => 'page#reserve', 'url' => '/reserve', 'verb' => 'POST'],
-    ]
-];
